89542529ffe16b6cc2c58c7a6a06c6e582eae6f0
nikolina <nikolinap85@gmail.com>
2019-03-22 16:33:22 +0100

    Test: are all enclosures shown on the home page using data fixtures (Doctrine)

---
+++ src/DataFixtures/ORM/LoadSecurityData.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures\ORM;
+
+use App\Entity\Enclosure;
+use App\Entity\Security;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadSecurityData extends Fixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $herbivorousEnclosure = $this->getReference('herbivorous-enclosure');
+
+        $this->addSecurity($herbivorousEnclosure, 'Fence', true);
+
+        $carnivorousEnclosure = $this->getReference('carnivorous-enclosure');
+
+        $this->addSecurity($carnivorousEnclosure, 'Electric fence', false);
+        $this->addSecurity($carnivorousEnclosure, 'Guard tower', false);
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 2;
+    }
+
+    private function addSecurity(Enclosure $enclosure, string $name, bool $isActive)
+    {
+        $enclosure->addSecurity(new Security($name, $isActive, $enclosure));
+    }
+}
--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,18 +2,25 @@
 
 namespace App\Tests\Controller;
 
+use App\DataFixtures\ORM\LoadBasicParkData;
+use App\DataFixtures\ORM\LoadSecurityData;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
     public function testEnclosuresAreShownOnHomepage()
     {
+        $this->loadFixtures([
+            LoadBasicParkData::class,
+            LoadSecurityData::class,
+        ]);
+
         $client = $this->makeClient();
 
         $crawler = $client->request('GET', '/lucky');
         $this->assertStatusCode(200, $client);
 
         $table = $crawler->filter('.table-enclosures');
-        $this->assertCount(1, $table->filter('tbody tr'));
+        $this->assertCount(3, $table->filter('tbody tr'));
     }
 }
